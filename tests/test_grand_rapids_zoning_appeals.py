ec80aff092090fc7fe239acf5fd4188d17a5d8c9
Hails <whatuphails@yahoo.com>
2023-07-09 10:29:34 -0400

    Add files via upload

--- tests/test_grand_rapids_zoning_appeals.py
+++ tests/test_grand_rapids_zoning_appeals.py
@@ -1,6 +1,8 @@
+from datetime import datetime
 from os.path import dirname, join
 
 import pytest
+from city_scrapers_core.constants import BOARD
 from city_scrapers_core.utils import file_response
 from freezegun import freeze_time
 
@@ -30,54 +32,79 @@ def test_tests():
     assert False
 """
 
-# def test_title():
-#     assert parsed_items[0]["title"] == "EXPECTED TITLE"
+
+def test_title():
+    assert parsed_items[0]["title"] == "Board of Zoning Appeals "
 
 
 # def test_description():
 #     assert parsed_items[0]["description"] == "EXPECTED DESCRIPTION"
 
 
-# def test_start():
-#     assert parsed_items[0]["start"] == datetime(2019, 1, 1, 0, 0)
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2023, 2, 16, 13, 0)
 
 
 # def test_end():
-#     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
+#     assert parsed_items[0]["end"] == datetime(2023, 2, 16, 15, 0)
 
 
 # def test_time_notes():
 #     assert parsed_items[0]["time_notes"] == "EXPECTED TIME NOTES"
 
 
-# def test_id():
-#     assert parsed_items[0]["id"] == "EXPECTED ID"
+def test_id():
+    assert (
+        parsed_items[0]["id"]
+        == "grand_rapids_zoning_appeals/202302161300/x/board_of_zoning_appeals"
+    )
 
 
-# def test_status():
-#     assert parsed_items[0]["status"] == "EXPECTED STATUS"
+def test_status():
+    assert parsed_items[0]["status"] == "passed"
 
 
-# def test_location():
-#     assert parsed_items[0]["location"] == {
-#         "name": "EXPECTED NAME",
-#         "address": "EXPECTED ADDRESS"
-#     }
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "",
+        "address": "Public Hearing Room, 2nd Floor 1120 Monroe Ave NW, Grand Rapids, MI  49503",  # noqa
+    }
 
 
-# def test_source():
-#     assert parsed_items[0]["source"] == "EXPECTED URL"
+def test_source():
+    assert (
+        parsed_items[0]["source"]
+        == "http://grandrapidscitymi.iqm2.com/Citizens/calendar.aspx?View=List"
+    )
 
 
-# def test_links():
-#     assert parsed_items[0]["links"] == [{
-#       "href": "EXPECTED HREF",
-#       "title": "EXPECTED TITLE"
-#     }]
+def test_links():
+    assert parsed_items[0]["links"] == [
+        {
+            "href": "http://grandrapidscitymi.iqm2.com//Citizens/Detail_Meeting.aspx?ID=6939",  # noqa
+            "title": "Meeting Page",
+        },
+        {
+            "href": "http://grandrapidscitymi.iqm2.com//Citizens/Detail_Meeting.aspx?ID=6939",  # noqa
+            "title": "Agenda",
+        },
+        {
+            "href": "http://grandrapidscitymi.iqm2.com/Citizens/FileOpen.aspx?Type=1&ID=5181&Inline=True",  # noqa
+            "title": "Agenda Packet",
+        },
+        {
+            "href": "http://grandrapidscitymi.iqm2.com/Citizens/FileView.aspx?Type=15&ID=5246",  # noqa
+            "title": "Summary",
+        },
+        {
+            "href": "http://grandrapidscitymi.iqm2.com/Citizens/FileView.aspx?Type=12&ID=5246",  # noqa
+            "title": "Minutes",
+        },
+    ]
 
 
-# def test_classification():
-#    assert parsed_items[0]["classification"] == BOARD
+def test_classification():
+    assert parsed_items[0]["classification"] == BOARD
 
 
 @pytest.mark.parametrize("item", parsed_items)
