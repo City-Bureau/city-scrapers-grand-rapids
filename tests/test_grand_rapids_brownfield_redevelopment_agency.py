9d17cc3f931e1140d94cea8c6d12012975f1c446
Hails <whatuphails@yahoo.com>
2023-07-22 23:56:42 -0400

    Add files via upload

---
+++ tests/test_grand_rapids_brownfield_redevelopment_agency.py
@@ -0,0 +1,110 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.grand_rapids_brownfield_redevelopment_agency import (
+    GrandRapidsBrownfieldRedevelopmentAgencySpider,
+)
+
+test_response = file_response(
+    join(
+        dirname(__file__), "files", "grand_rapids_brownfield_redevelopment_agency.html"
+    ),
+    url="http://grandrapidscitymi.iqm2.com/Citizens/calendar.aspx?View=List",
+)
+spider = GrandRapidsBrownfieldRedevelopmentAgencySpider()
+
+freezer = freeze_time("2023-07-22")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+"""
+Uncomment below
+def test_tests():
+    print("Please write some tests for this spider or at least disable this one.")
+    assert False
+"""
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Brownfield Redevelopment Authority"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2023, 1, 25, 8, 30)
+
+
+# def test_end():
+#     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert (
+        parsed_items[0]["id"]
+        == "grand_rapids_brownfield_redevelopment_agency/202301250830/x/brownfield_redevelopment_authority"  # noqa
+    )
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "passed"
+
+
+# def test_location():
+#     assert parsed_items[0]["location"] == {
+#         "name": "",
+#         "address": "City Commission Chambers City Hall, 300 Monroe Ave 'NW, 9th Floor, Grand Rapids, MI  49503"  # noqa
+#     }
+
+
+def test_source():
+    assert (
+        parsed_items[0]["source"]
+        == "http://grandrapidscitymi.iqm2.com/Citizens/calendar.aspx?View=List"
+    )
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [
+        {
+            "href": "http://grandrapidscitymi.iqm2.com//Citizens/Detail_Meeting.aspx?ID=7119",  # noqa
+            "title": "Meeting Page",
+        },
+        {
+            "href": "http://grandrapidscitymi.iqm2.com//Citizens/Detail_Meeting.aspx?ID=7119",  # noqa
+            "title": "Agenda",
+        },
+        {
+            "href": "http://grandrapidscitymi.iqm2.com/Citizens/FileOpen.aspx?Type=1&ID=5158&Inline=True",  # noqa
+            "title": "Agenda Packet",
+        },
+        {"href": None, "title": "Summary"},
+        {
+            "href": "http://grandrapidscitymi.iqm2.com/Citizens/FileOpen.aspx?Type=12&ID=5216&Inline=True",  # noqa
+            "title": "Minutes",
+        },
+    ]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == NOT_CLASSIFIED
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
