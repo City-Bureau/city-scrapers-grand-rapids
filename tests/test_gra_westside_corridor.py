0d0a50fb8be46b8b947ea0972d5bff99817f8e8b
Hails <whatuphails@yahoo.com>
2023-08-12 05:50:05 -0400

    Add files via upload

---
+++ tests/test_gra_westside_corridor.py
@@ -0,0 +1,106 @@
+from datetime import datetime
+from os.path import dirname, join
+
+import pytest
+from city_scrapers_core.constants import NOT_CLASSIFIED
+from city_scrapers_core.utils import file_response
+from freezegun import freeze_time
+
+from city_scrapers.spiders.gra_westside_corridor import GraWestsideCorridorSpider
+
+test_response = file_response(
+    join(dirname(__file__), "files", "gra_westside_corridor.html"),
+    url="http://grandrapidscitymi.iqm2.com/Citizens/calendar.aspx",
+)
+spider = GraWestsideCorridorSpider()
+
+freezer = freeze_time("2023-08-12")
+freezer.start()
+
+parsed_items = [item for item in spider.parse(test_response)]
+
+freezer.stop()
+
+
+"""
+Uncomment below
+def test_tests():
+    print("Please write some tests for this spider or at least disable this one.")
+    assert False
+"""
+
+
+def test_title():
+    assert parsed_items[0]["title"] == "Westside Corridor Improvement Authority"
+
+
+def test_description():
+    assert parsed_items[0]["description"] == ""
+
+
+def test_start():
+    assert parsed_items[0]["start"] == datetime(2023, 2, 3, 9, 0)
+
+
+# def test_end():
+#     assert parsed_items[0]["end"] == datetime(2019, 1, 1, 0, 0)
+
+
+def test_time_notes():
+    assert parsed_items[0]["time_notes"] == ""
+
+
+def test_id():
+    assert (
+        parsed_items[0]["id"]
+        == "gra_westside_corridor/202302030900/x/westside_corridor_improvement_authority"  # noqa
+    )
+
+
+def test_status():
+    assert parsed_items[0]["status"] == "passed"
+
+
+def test_location():
+    assert parsed_items[0]["location"] == {
+        "name": "",
+        "address": "415 Leonard Street NW 415 Leonard Street NW, Grand Rapids, MI  49504",  # noqa
+    }
+
+
+def test_source():
+    assert (
+        parsed_items[0]["source"]
+        == "http://grandrapidscitymi.iqm2.com/Citizens/calendar.aspx"
+    )
+
+
+def test_links():
+    assert parsed_items[0]["links"] == [
+        {
+            "href": "http://grandrapidscitymi.iqm2.com//Citizens/Detail_Meeting.aspx?ID=7199",  # noqa
+            "title": "Meeting Page",
+        },
+        {
+            "href": "http://grandrapidscitymi.iqm2.com//Citizens/Detail_Meeting.aspx?ID=7199",  # noqa
+            "title": "Agenda",
+        },
+        {
+            "href": "http://grandrapidscitymi.iqm2.com/Citizens/FileOpen.aspx?Type=1&ID=5170&Inline=True",  # noqa
+            "title": "Agenda Packet",
+        },
+        {"href": None, "title": "Summary"},
+        {
+            "href": "http://grandrapidscitymi.iqm2.com/Citizens/FileView.aspx?Type=12&ID=5227",  # noqa
+            "title": "Minutes",
+        },
+    ]
+
+
+def test_classification():
+    assert parsed_items[0]["classification"] == NOT_CLASSIFIED
+
+
+@pytest.mark.parametrize("item", parsed_items)
+def test_all_day(item):
+    assert item["all_day"] is False
